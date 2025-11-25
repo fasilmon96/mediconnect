373264c6b2dac41e88fffae6a6471539f03861b2
HP <fasilrahman96@gmail.com>
2025-11-25 12:33:18 +0530

    added

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,15 +27,16 @@ const PORT = ENV.PORT || 5000 ;
  
 
 
-
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist",)));
   
   app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    res.sendFile(path.join(__dirname, "..frontend","dist","index.html"));
   });
 
 }
+
+
 
 
 app.listen(PORT , () => {
